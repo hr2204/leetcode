8a52b5d7bdce0ff083ddb133cbf4f396efee100b
Ren He <rhe@Rens-MacBook-Pro-2.local>
2016-08-03 11:40:19 -0700

    set example

---
+++ solutions/217_contains_duplicate.py
@@ -0,0 +1,26 @@
+# 217. Contains Duplicate
+# Difficulty: Easy
+# Given an array of integers, find if the array contains any duplicates. Your function should retur
+# n true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Solution 1: Time Limit Exceeded
+        # if len(nums) == 0 or len(nums) == 1: return False
+        # for i,num in enumerate(nums):
+        #     if num in nums[i+1:]:
+        #         return True
+        #
+        # return False
+
+        # Solution 2: Correct
+        return len(nums)!= len(set(nums))
+
+
+
+s = Solution()
+print s.containsDuplicate([1,2,3,1])
