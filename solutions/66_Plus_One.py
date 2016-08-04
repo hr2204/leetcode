64f3009b7baf3bee0a1ccfb28dd009265b05b3b7
hr2204 <hr2204@hotmail.com>
2016-08-03 23:12:36 -0700

    update no recursion solution

--- solutions/66_Plus_One.py
+++ solutions/66_Plus_One.py
@@ -14,16 +14,41 @@ class Solution(object):
         if len(digits) <= 0:
             return []
         if len(digits) == 1:
-            if digits[0]!=9:
+            if digits[0] != 9:
                 digits[0] = digits[0] + 1
                 return digits
             else:
-                return [1,0]
+                return [1, 0]
         else:
             if digits[-1] != 9:
                 digits[-1] = digits[-1] + 1
                 return digits
             else:
                 return self.plusOne(digits[:-1]) + [0]
+
+    def plusOne(digits):
+        num = 0
+        for i in range(len(digits)):
+            num += digits[i] * pow(10, (len(digits) - 1 - i))
+        return [int(i) for i in str(num + 1)]
+
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            if digits[i] < 10:
+                carry = 0
+                break
+            else:
+                digits[i] -= 10
+        if carry == 1:
+            digits.insert(0, 1)
+        return digits
+
+
 if __name__ == "__main__":
-    assert Solution().plusOne([1,2,9]) == [1,3,0]
+    assert Solution().plusOne([1, 2, 9]) == [1, 3, 0]
