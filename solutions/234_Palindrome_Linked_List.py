7af3ac5b5bbbba14d824c5d7d9a41a721e084701
Ren He <rhe@Rens-MacBook-Pro-2.local>
2016-08-12 14:31:44 -0700

    add reverse list method solution

--- solutions/234_Palindrome_Linked_List.py
+++ solutions/234_Palindrome_Linked_List.py
@@ -12,11 +12,41 @@
 #         self.next = None
 
 from data_structure.list_node import ListNode
+from utilities.linked_list import generate_list, printNode
 class Solution(object):
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
+        orgin = list()
+        p0 = head
+        while head:
+            orgin.append(head.val)
+            head = head.next
+        head = self.reverseList(p0)
+        i = 0
+        printNode(head)
+        while head:
+            if head.val!=orgin[i]:
+                return False
+            head = head.next
+            i += 1
+        return True
+
+    def reverseList(self,head):
+        if head is None:
+            return
+        pre = None
+        while head:
+            next = head.next
+            head.next = pre
+            pre = head
+            head = next
+        return pre
+
+if __name__ == "__main__":
+    a = generate_list([1,2,3,1])
+    print Solution().isPalindrome(a)
 
 
