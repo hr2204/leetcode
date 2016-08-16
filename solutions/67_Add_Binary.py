06c9f84caabbad11cfc2b5f136e97aa7f4c5be5f
Ren He <hr2204@hotmail.com>
2016-08-16 11:26:48 -0700

    first solution

--- solutions/67_Add_Binary.py
+++ solutions/67_Add_Binary.py
@@ -24,13 +24,29 @@ class Solution(object):
         i = 1
         carry = 0
         while n > 0:
-            digitA = a[-i]
-            digitB = b[-i]
+            digitA = int(a[-i])
+            digitB = int(b[-i])
             res += str((carry + digitA + digitB)%2)
             carry = (carry + digitA + digitB)/2
             i += 1
             n -= 1
+        # if len(a) == len(b):
+        #     res += str(carry)
 
-        return res
+        while i<=len(a):
+            digitA = int(a[-i])
+            res += str((carry + digitA )%2)
+            carry = (carry + digitA)/2
+            i += 1
+        while i<=len(b):
+            digitB = int(b[-i])
+            res += str((carry + digitB )%2)
+            carry = (carry + digitB)/2
+            i += 1
+        if carry:
+            res += str(carry)
+        return res[::-1]
+
+
 if __name__ == '__main__':
-    print Solution().addBinary("11111","1011")
+    print Solution().addBinary("10","111")
