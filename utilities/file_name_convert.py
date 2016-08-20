a7b2d0e7032b77002a74d5e186389a53880b0bf1
Ren He <hr2204@hotmail.com>
2016-08-19 17:00:27 -0700

    first solution

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = "     58. Length of Last Word  "
+fileName = "    165. Compare Version Numbers   "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
