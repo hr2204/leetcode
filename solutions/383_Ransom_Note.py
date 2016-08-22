78530cb65c07a0d1d8e269b2b6ba4d717f21f015
Ren He <hr2204@hotmail.com>
2016-08-22 11:28:16 -0700

    first solution

--- solutions/383_Ransom_Note.py
+++ solutions/383_Ransom_Note.py
@@ -1,6 +1,6 @@
 # 383. Ransom Note  QuestionEditorial
 # Difficulty: Easy
-#  Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from  all  the  magazines, 
+#  Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from  all  the  magazines,
 # write  a  function  that  will  return  true  if  the  ransom   note  can  be  constructed  from  the  magazines ;
 #   otherwise,  it  will  return  false.   
 #
@@ -20,6 +20,20 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        charDict = dict()
+        for char in magazine:
+            if char in charDict:
+                charDict[char] += 1
+            else:
+                charDict[char] = 1
+        for char in ransomNote:
+            if char in charDict and charDict[char] > 0:
+                charDict[char] -= 1
+            else:
+                return False
+        return True
+
+
 
 if __name__ == '__main__':
     print Solution().canConstruct("a","b")
