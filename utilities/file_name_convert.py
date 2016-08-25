23fc5e26801235d5e3bf0c8b2d5fc5b34a5c246c
Ren He <hr2204@hotmail.com>
2016-08-25 13:45:57 -0700

    first solution

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = "   303. Range Sum Query - Immutable  "
+fileName = "  155. Min Stack "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
