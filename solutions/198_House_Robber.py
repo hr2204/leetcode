c06ac2f4f544e0b8b7043c1c7698bdff2150b101
Ren He <hr2204@hotmail.com>
2016-08-25 09:43:39 -0700

    first pass solution

--- solutions/198_House_Robber.py
+++ solutions/198_House_Robber.py
@@ -1,4 +1,4 @@
-# 198. House Robber  Q
+# 198. House Robber
 # Difficulty: Easy
 # You are a professional robber planning to rob houses along a street.
 #  Each house has a certain amount of money stashed, the only constraint
@@ -10,13 +10,25 @@
 # determine the maximum amount of money you can rob tonight without alerting the police.
 #
 
-
 class Solution(object):
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-2] + nums[i],dp[i-1])
+
+        return dp[i]
 
 
 
