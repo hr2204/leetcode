89561e1654ed5225032983e97299e06a8869a433
Ren He <hr2204@hotmail.com>
2016-09-01 18:20:46 -0700

    first solution

--- solutions/104_Maximum_Depth_of_Binary_Tree.py
+++ solutions/104_Maximum_Depth_of_Binary_Tree.py
@@ -31,7 +31,7 @@ class Solution(object):
         if not node.left and not node.right:
             return depth
         if node.left and node.right:
-            return min(self.helper(node.left,depth),self.helper(node.right,depth))
+            return max(self.helper(node.left,depth),self.helper(node.right,depth))
         if node.left:
             return self.helper(node.left,depth)
         if node.right:
