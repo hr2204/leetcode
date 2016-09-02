973a5af04d9fc456fb8405200543ff7a1a4c6486
Ren He <hr2204@hotmail.com>
2016-09-02 08:41:06 -0700

    second solution

--- solutions/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ solutions/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -31,6 +31,21 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+
+        if p.val<root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val>root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
         if p == root:
             return p
         if q == root:
