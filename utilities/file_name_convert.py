b6d358919728b63a874f8f24808ac64b01cf183f
Ren He <hr2204@hotmail.com>
2016-11-14 17:42:00 -0800

    first solution

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = " 404. Sum of Left Leaves    "
+fileName = "     423. Reconstruct Original Digits from English    "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
