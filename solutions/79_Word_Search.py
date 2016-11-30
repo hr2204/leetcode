ade778e2b43810fc5c5829a418d124cc98ba77ce
Ren He <hr2204@hotmail.com>
2016-11-30 15:59:22 -0800

    update passed solution

--- solutions/79_Word_Search.py
+++ solutions/79_Word_Search.py
@@ -19,7 +19,6 @@
 # word = "ABCCED", -> returns true,
 # word = "SEE", -> returns true,
 # word = "ABCB", -> returns false.
-import copy
 class Solution(object):
     def exist(self, board, word):
         """
@@ -27,45 +26,43 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        flag = [False]
         visited = []
+        self.flag = False
         for row in range(len(board)):
             visited.append([0]*len(board[0]))
 
         for row in range(len(board)):
             for col in range(len(board[row])):
                 if board[row][col] == word[0]:
-                    self.dfs(board,word,visited,row,col,flag)
-                if flag[0]:
+                    self.dfs(board,word,visited,row,col,self.flag,0)
+                if self.flag:
                     return True
 
         return False
 
-    def dfs(self,board,word,visited,row,col,flag):
-
+    def dfs(self,board,word,visited,row,col,flag,word_idx):
+        if self.flag:
+            return
 
 
         if row<0 or col < 0 or row >=len(board) or col >= len(board[0]):
             return
         if visited[row][col]:
             return
+        if board[row][col] != word[word_idx]:
+            return
 
-        if len(word) == 1 and board[row][col] == word[0]:
-            flag[0] = True
+        if word_idx == len(word)-1:
+            self.flag = True
             return
-        if board[row][col] != word[0]:
-            return
+
 
 
         visited[row][col] = 1
-
-        self.dfs(board,word[1:],visited,row-1,col,flag)
-
-        self.dfs(board,word[1:],visited,row+1,col,flag)
-
-        self.dfs(board,word[1:],visited,row,col-1,flag)
-
-        self.dfs(board,word[1:],visited,row,col+1,flag)
+        self.dfs(board,word,visited,row,col-1,flag,word_idx+1)
+        self.dfs(board,word,visited,row-1,col,flag,word_idx+1)
+        self.dfs(board,word,visited,row,col+1,flag,word_idx+1)
+        self.dfs(board,word,visited,row+1,col,flag,word_idx+1)
 
         visited[row][col] = 0
 
