6549ffa5112fb14a4e38a0deaa2f5d9eb2e7a195
Ren He <hr2204@hotmail.com>
2016-12-06 17:38:13 -0800

    not done

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = "     390. Elimination Game               "
+fileName = "    279. Perfect Squares               "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
