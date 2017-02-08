971913861f92755a8270cfa5dc967b0143df92bf
rhe <hr2204@hotmail.com>
2017-02-08 10:03:34 -0800

    first solution

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = "    15. 3Sum              "
+fileName = "    33. Search in Rotated Sorted Array              "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
