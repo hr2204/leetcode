c11ca7ed9781527a67963c1e5be7d9af9b0d24f5
rhe <hr2204@hotmail.com>
2017-03-29 15:46:18 -0700

    dp solution

--- solutions/221_Maximal_Square.py
+++ solutions/221_Maximal_Square.py
@@ -17,12 +17,26 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
+        if not matrix:
+            return 0
+        m = len(matrix)
+        n = len(matrix[0])
+
+        dp = [[0 if matrix[i][j] == '0' else 1 for j in xrange(n)] for i in xrange(m)]
+
+        for i in xrange(1,m):
+            for j in xrange(1,n):
+                if matrix[i][j] != '0':
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) + 1
+        return max([max(i) for i in dp]) ** 2
+
+
 
 
 
 if __name__ == '__main__':
-    input = [[1,0,1,0,0],
-             [1,0,1,1,1],
-             [1,1,1,1,1],
-             [1,0,0,1,0]]
+    input = [['1','0','1','0','0'],
+             ['1','0','1','1','1'],
+             ['1','1','1','1','1'],
+             ['1','0','0','1','0']]
     print Solution().maximalSquare(input)
