0f1201374e2a89439bdc960ad49bb3d1dfa17253
rhe <hr2204@hotmail.com>
2017-04-03 16:00:45 -0700

    divide and conquer method

--- solutions/53_Maximum_Subarray.py
+++ solutions/53_Maximum_Subarray.py
@@ -24,6 +24,35 @@ class Solution(object):
             maxSum = max(maxSum,curSum)
         return maxSum
 
+
+
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return self.helper(nums, 0, len(nums) - 1)
+
+    def helper(self, nums, l, r):
+        if l > r:
+            return -2147483648
+
+        m = l + (r - l) / 2
+        left = self.helper(nums, l, m - 1)
+        right = self.helper(nums, m + 1, r)
+
+        leftMax = a = 0
+        for i in range(m - 1, l - 1, -1):
+            a += nums[i]
+            leftMax = max(leftMax, a)
+
+        rightMax = b = 0
+        for i in range(m + 1, r + 1):
+            b += nums[i]
+            rightMax = max(rightMax, b)
+
+        return max(leftMax + nums[m] + rightMax, max(left, right))
+
 if __name__ == '__main__':
     nums =  [-2,1,-3,4,-1,2,1,-5,4]
     print Solution().maxSubArray(nums)
