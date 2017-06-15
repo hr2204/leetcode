2fb45e86a24a6c6d4aa8cd4ec7ab9efdd9c70d6f
hr2204 <hr2204@hotmail.com>
2017-06-15 09:24:19 -0700

    first solution

--- utilities/file_name_convert.py
+++ utilities/file_name_convert.py
@@ -1,6 +1,6 @@
 
 
-fileName = "   96. Unique Binary Search Trees            "
+fileName = "      122. Best Time to Buy and Sell Stock II            "
 fileName = fileName.strip()
 fileName = fileName.replace(" ","_")
 fileName = fileName.replace(".","")
