72ec846b73ff8184d2bc5d11948f3e4f917c13b6
hr2204 <hr2204@hotmail.com>
2018-06-19 22:37:05 -0700

    first solution

---
+++ solutions_by_company/google/66_Plus_One.py
@@ -0,0 +1,39 @@
+# 66. Plus One
+# Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+#
+# The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+#
+# You may assume the integer does not contain any leading zero, except the number 0 itself.
+#
+# Example 1:
+#
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Example 2:
+#
+# Input: [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        res = []
+
+        if not digits:
+            return res
+        temp = 0
+        for num in digits:
+            temp = temp * 10 + num
+
+        temp = temp + 1
+
+        while temp:
+            res.append(temp % 10)
+            temp = temp / 10
+
+        return res[::-1]
